e5f6e5c690a72119d396d25d68e21d02aca6c0ca
AndrMoura <andre_ptos29@msn.com>
2022-06-14 12:45:02 +0100

    Update morpher.py

--- facemorpher/morpher.py
+++ facemorpher/morpher.py
@@ -113,7 +113,8 @@ def morph(src_img, src_points, dest_img, dest_points,
       if background == 'average':
         average_background = blender.weighted_average(src_img, dest_img, percent)
         average_face = blender.overlay_image(average_face, mask, average_background)
-
+        average_face = average_face.astype(np.uint8)
+ 
     plt.plot_one(average_face)
     plt.save(average_face)
     video.write(average_face)
